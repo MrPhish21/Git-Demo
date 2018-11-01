dc0451fcee7fee54eb98157ee18232b203ebe644
Nash Salmon <Nash.Salmon@iam.local>
2018-11-01 17:58:04 -0500

    tip calc 1

---
+++ wpfTipCalculator8000/wpfTipCalculator8000/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace wpfTipCalculator8000
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void calculateBtn_Click(object sender, RoutedEventArgs e)
+        {
+            double billTotal = Convert.ToDouble(totalBox.Text);
+            double tipAmount = .15;
+            double result = 0;
+
+            result = (billTotal * tipAmount) + billTotal;
+
+            resultsTxtBk.Text = "Your bill total will be $" + Math.Round(result, 2);
+
+        }
+    }
+}
